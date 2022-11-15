0b0bc7560beffb96ad81303800e71645539a0ded
rafael rouillat <rafael.rouillat@telecom-sudparis.eu>
2022-11-15 22:19:03 +0100

    twig inventaire complet

--- src/Controller/ReserveController.php
+++ src/Controller/ReserveController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Boardgame;
 use App\Entity\Reserve;
+use App\Controller\Actions;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,20 +22,6 @@ class ReserveController extends AbstractController
      */
     public function index(): Response
     {
-//         return new Response(
-//             '<html>
-//     <body> Bienvenue dans notre réserve !
-//         <ul>
-//             <li>Jetez un oeil aux jeux présents dans les réserves</li>
-//             <li>Demandez des précisions aux joueurs</li>
-//         </ul>
-//     </body>
-// </html>'
-
-//     );
-//     return $this->render('reserve/index.html.twig', [
-//         'controller_name' => 'ReserveController',
-//     ]);
 
     return $this->render('index.html.twig',
         [ 'welcome' => "Accueil des réserves" ]
@@ -49,33 +36,6 @@ class ReserveController extends AbstractController
      */
     public function listAction(ManagerRegistry $doctrine): Response
     {
-//         $htmlpage = '<!DOCTYPE html>
-// <html>
-//     <head>
-//         <meta charset="UTF-8">
-//         <title>Réserves</title>
-//     </head>
-//     <body>
-//         <h1>Réserves</h1>
-//         <p>Voici les reserves partagées sur notre site:</p>
-//         <ul>';
-        
-//         $entityManager= $doctrine->getManager();
-//         $reserves = $entityManager->getRepository(Reserve::class)->findAll();
-//         foreach($reserves as $reserve) {
-//            $htmlpage .= '<li>
-//             <a href="/reserve/'.$reserve->getid().'">'.$reserve->getName().'</a></li>';
-//          }
-
-//         $htmlpage .= '</ul>';
-
-//         $htmlpage .= '</body></html>';
-        
-//         return new Response(
-//             $htmlpage,
-//             Response::HTTP_OK,
-//             array('content-type' => 'text/html')
-//             );
     $entityManager= $doctrine->getManager();
     $reserves = $entityManager->getRepository(Reserve::class)->findAll();
 
@@ -95,43 +55,28 @@ class ReserveController extends AbstractController
  *    
  * @param Integer $id
  */
-    public function showAction(Reserve $reserve): Response
+    public function showAction(ManagerRegistry $doctrine, Reserve $reserve): Response
 {
-       
-    //     $reserveRepo = $doctrine->getRepository(reserve::class);
-    //     $reserve = $reserveRepo->find($id);
+    $entityManager= $doctrine->getManager();
+    $boardgames = $entityManager->getRepository(Boardgame::class)->findBy(array('reserve' => $reserve->getId()));
 
-    //     if (!$reserve) {
-    //       throw $this->createNotFoundException('The reserve does not exist');
-    //  }
-
-    //     $res = '<!DOCTYPE html>
-    //     <html>
-    //      <head>
-    //          <meta charset="UTF-8">
-    //          <title>Boardgames</title>
-    //      </head>
-    //      <body>
-    //          <h1>Jeux de société</h1>
-    //          <p>Voici les jeux contenus dans la réserve '. '<Strong>' . $reserve->getName() . '</Strong>' . ':</p>
-    //          <ul><dl>';
-
-    //         foreach($reserve->getBoardgame() as $boardgame) {
-    //             $res .= '<dd>' . '<Strong>' . $boardgame->getName() .'</Strong>'. ', ' . $boardgame->getType() . ', difficulté ' . $boardgame->getDifficulty() . ', sorti en' . $boardgame->getYear() . '</dd>';
-    //         }
-
-    //     $res .= '</dl>';
-    //     $res .= '</ul></body></html>';
-
-
-    //     $res .= '<p/><a href="' . $this->generateUrl('app_reserve') . '">Back</a>';
-
-    //     return new Response('<html><body>'. $res . '</body></html>');
-
+    dump($boardgames);
+   
     return $this->render('reserve/show.html.twig',
-    [ 'reserve' => $reserve ]
+    [ 
+        'boardgames' => $boardgames,
+        'reserve' => $reserve
+    ]
     );
 }
+
+// public function configureActions(Actions $actions): Actions
+//     {
+
+//     return $actions
+//         ->add(Crud::PAGE_INDEX, Action::DETAIL)
+//     ;
+//     }
 
 
 }
